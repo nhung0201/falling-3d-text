924f4aa3af41bf5c0b895d0c474f3536ea188566
Wainii Dev <wainii1998@gmail.com>
2025-05-30 18:54:25 +0700

    Update codecode

--- js/app.js
+++ js/app.js
@@ -9,6 +9,14 @@ const texts = [
     "Je t'aime",
 ];
 
+let images = [
+    "https://i.pinimg.com/736x/77/86/1a/77861aa910507e41754f68b5d60699b6.jpg",
+    "https://i.pinimg.com/736x/c3/84/9e/c3849e2eba19a1e8155b7464f9b89da8.jpg",
+    "https://i.pinimg.com/736x/5f/3c/71/5f3c71b061d9d56cb00e5fc1dd9f5969.jpg",
+    "https://i.pinimg.com/736x/94/59/66/9459664d0858ec81ba249d98fdb4ee3c.jpg",
+    "https://i.pinimg.com/736x/d9/09/c0/d909c0b86ea1a4e65495785db03ef636.jpg"
+];
+
 const scene = document.getElementById("scene");
 let rotateX = 0, rotateY = 0;
 let targetRotateX = 0, targetRotateY = 0;
@@ -84,13 +92,6 @@ function createFallingText(initial = false) {
 
 
 function createHeart(initial = false, initialY = -50) {
-    let images = [
-        "https://i.pinimg.com/736x/77/86/1a/77861aa910507e41754f68b5d60699b6.jpg",
-        "https://i.pinimg.com/736x/c3/84/9e/c3849e2eba19a1e8155b7464f9b89da8.jpg",
-        "https://i.pinimg.com/736x/5f/3c/71/5f3c71b061d9d56cb00e5fc1dd9f5969.jpg",
-        "https://i.pinimg.com/736x/94/59/66/9459664d0858ec81ba249d98fdb4ee3c.jpg",
-        "https://i.pinimg.com/736x/d9/09/c0/d909c0b86ea1a4e65495785db03ef636.jpg"
-    ];
     const heart = document.createElement("div");
     heart.className = "heart";
     heart.innerHTML = `<img src="${images[Math.floor(Math.random() * images.length)]}" alt="♡" />`;//"♡";
